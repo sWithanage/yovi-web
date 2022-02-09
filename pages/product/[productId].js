6a68bc5f5a27947ff04e15dfd7cc3788a8305b6a
Sasanka Withanage <48270130+sWithanage@users.noreply.github.com>
2022-02-09 16:01:44 +0530

    Product update

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -6,22 +6,34 @@ const myLoader = ({ src, width, quality }) => {
     return `https://reqres.in/img/faces/2-image.jpg`
 }
 
-export default function Product({data}) {
-    console.log(data.data?.avatar);
+function Product({props}) {
+    const data = JSON.parse(props.data);
+    console.log(data);
     return (
         <div className={styles.container}>
             <Head>
-                <title>{data.data.first_name} {data.data.last_name}  - YOVI Clothing</title>
-                <meta name="description" content={data.support.text} />
+                <title>{data.data[props.productId].name} {data.data[props.productId].name}  - YOVI Clothing</title>
+                <meta name="description" content={data.data[props.productId].name} />
+                <meta name="description" content={data.data[props.productId].name}/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://yovi.vercel.app/productVege/1"/>
+                <meta property="og:title" content={data.data[props.productId].name}/>
+                <meta property="og:description" content={data.data[props.productId].name}/>
+                <meta property="og:image" content={data.data[props.productId].imageUrl}/>
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://yovi.vercel.app/productVege/1"/>
+                <meta property="twitter:title" content={data.data[props.productId].name}/>
+                <meta property="twitter:description" content={data.data[props.productId].name}/>
+                <meta property="twitter:image" content={data.data[props.productId].imageUrl}/>
             </Head>
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Hello {data.data.first_name}
+                    Hello {data.data[props.productId].name}
                 </h1>
 
                 <Image
-                    src={data.data?.avatar}
+                    src={data.data[props.productId].imageUrl}
                     alt="Picture of the author"
                     width={200}
                     height={200}
@@ -32,25 +44,39 @@ export default function Product({data}) {
     )
 }
 
-export async function getServerSideProps({params}) {
-    const res = await fetch(`https://reqres.in/api/users/`+params?.productId)
+Product.getInitialProps = async (ctx) => {
+    const res = await fetch(`https://sun-shinevege-mart.herokuapp.com/api/item`)
     const data = await res.json()
-    console.log(data);
-    if(data?.data){
-        return { props: { data }}
-    } else {
-        return { props: { "data": {
-                    "data": {
-                        "id": 1250,
-                        "email": "Not Found",
-                        "first_name": "Not Found",
-                        "last_name": "Not Found",
-                        "avatar": "https://reqres.in/img/faces/2-image.jpg"
-                    },
-                    "support": {
-                        "url": "https://reqres.in/#support-heading",
-                        "text": "Not Found. Please find this one."
-                    }
-                } }}
+    return {
+        props: {
+            productId: ctx.query.productId,
+            data: JSON.stringify(data)
+        }
     }
 }
+
+// Product.getInitialProps = async (ctx) => {
+//     const res = await fetch(`https://sun-shinevege-mart.herokuapp.com/api/item`)
+//     const data = await res.json();
+//     const productId = ctx.query.productId;
+//     console.log(data);
+//     if(data?.data){
+//         return { props: { data , productId}}
+//     } else {
+//         return { props: { "data": {
+//                     "data": {
+//                         "id": 1250,
+//                         "email": "Not Found",
+//                         "first_name": "Not Found",
+//                         "last_name": "Not Found",
+//                         "avatar": "https://reqres.in/img/faces/2-image.jpg"
+//                     },
+//                     "support": {
+//                         "url": "https://reqres.in/#support-heading",
+//                         "text": "Not Found. Please find this one."
+//                     }
+//                 } }}
+//     }
+// }
+
+export default Product
